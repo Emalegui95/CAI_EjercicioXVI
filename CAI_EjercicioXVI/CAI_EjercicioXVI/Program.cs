614827a1832b630be9c5e1b4e59e3b2a0e09e193
Emmanuel Leguizamon <62218305+Emalegui95@users.noreply.github.com>
2020-04-03 18:43:42 -0300

    Ejercicio XVI

---
+++ CAI_EjercicioXVI/CAI_EjercicioXVI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_EjercicioXVI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número: ");
+            string num = Console.ReadLine();
+
+
+            Console.WriteLine("El número " + num + " tiene " + num.Length + " dígitos");
+            Console.ReadKey(true);
+        }
+    }
+}
+
